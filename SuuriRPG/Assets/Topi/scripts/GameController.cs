2820258af85fd98859d0288971ce04acbb448f10
Kuutti <topi.kuutti@luukku.com>
2014-09-17 19:47:53 +0300

    Unity

---
+++ SuuriRPG/Assets/Topi/scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject pelaaja;
+	public GameObject vihollinen;
+
+	void Start () {
+		Instantiate (pelaaja, new Vector3 (-4, 1, 0), new Quaternion ());
+		Instantiate(vihollinen, new Vector3(4,1,0),new Quaternion());
+	}
+
+	void Update (){
+		StartCoroutine (Tappelu());
+	}
+
+	IEnumerator Tappelu(){
+		bool juu = GameObject.Find ("Pelaaja").GetComponent<Agent> ().valittu;
+		print (juu);
+		while (!juu){
+			Debug.Log ("2");
+			juu = GameObject.Find ("Pelaaja").GetComponent<Agent> ().valittu;
+			yield return null;
+		}
+
+		//while (!vihollinen.GetComponent("valittu")) {
+		//	yield return null;
+		//}
+		//Debug.Log ("2");
+
+	}
+
+	
+
+}
