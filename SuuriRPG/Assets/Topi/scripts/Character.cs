f8f059ea7d7a5038fdabb331489e40bd6d8be838
Kuutti <topi.kuutti@luukku.com>
2014-09-25 14:25:50 +0300

    Unity

---
+++ SuuriRPG/Assets/Topi/scripts/Character.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public bool valmis;
+	public bool valittu;
+	public ISkill ChosenSkill=null;
+
+	void Start(){
+		valmis = true;
+	}
+
+	public IEnumerator Move(Vector3 vector){     //liikutetaan kohde tiettyyn pisteeseen
+		valmis = false;
+		while(Vector3.Distance(transform.position, vector) > 1.5f)       //kuinka lähelle mennään
+			{
+			transform.position = Vector3.Lerp(transform.position, vector, 1f * Time.deltaTime);  //liikkuttaa smoothisti hahmoa
+			if (valmis){
+			yield break;
+			} else {
+			yield return null;
+				}
+			}
+		valmis=true;
+		}
+
+	void OnTriggerEnter(Collider other){   //pitäisi pysäyttää ekaan ei-kaveriin;
+		if(other.tag!=this.tag){
+		valmis = true;
+		}
+		}
+
+	void OnMouseDown(){
+		valittu = true;
+	}
+
+	public IEnumerator ChooseSkill(){
+		yield return null;
+	}
+
+	public ISkill GetSkill(){
+		return ChosenSkill;
+	}
+
+}
